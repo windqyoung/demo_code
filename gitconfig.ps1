57ed256d6b9140e43db2123a82fd19304424294b
windqyoung <txcses@gmail.com>
2018-07-02 10:45:34 +0800

    添加设置 core.filemode=false

--- gitconfig.ps1
+++ gitconfig.ps1
@@ -6,9 +6,7 @@ git config --global --add alias.ci "commit -v"
 git config --global --add alias.log1 "log --oneline"
 git config --global --add alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cI) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global --add alias.su "submodule"
-git config --global --add core.autocrlf "input"
 git config --global --add alias.st "status"
-git config --global --add core.symlinks "false"
 git config --global --add alias.dh "diff HEAD"
 git config --global --add alias.desc "describe"
 git config --global --add alias.sm "submodule"
@@ -18,6 +16,10 @@ git config --global --add alias.br "branch -v"
 git config --global --add alias.dc "diff --cached"
 git config --global --add alias.bi "bisect"
 git config --global --add alias.dfw "diff -w"
+
+git config --global --add core.filemode false
+git config --global --add core.autocrlf "input"
+git config --global --add core.symlinks "false"
 
 git config --global tag.sort version:refname
 git config --global --add alias.tv  "tag --sort=version:refname"
